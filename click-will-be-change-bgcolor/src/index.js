e813dae92109cbebacaeb62fbc8c66affd8d42c0
Eautalent <69576241+hajun96@users.noreply.github.com>
2022-01-13 16:35:09 +0900

    click-will-be-change-bgcolor

---
+++ click-will-be-change-bgcolor/src/index.js
@@ -0,0 +1,32 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34"
+];
+const ColorBody = document.querySelector("body");
+const changeBtn = document.querySelector("button");
+const fristColor = colors[Math.ceil(Math.random() * colors.length)];
+const secoundColor = colors[Math.ceil(Math.random() * colors.length)];
+
+function colorChange() {
+  const fristColor = colors[Math.ceil(Math.random() * colors.length)];
+  const secoundColor = colors[Math.ceil(Math.random() * colors.length)];
+
+  ColorBody.style.backgroundImage = `linear-gradient(to right ,${fristColor}, ${secoundColor})`;
+}
+changeBtn.addEventListener("click", colorChange);
